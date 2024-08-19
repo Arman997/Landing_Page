3bcc4b5fc3c5629df97cd28c6adcd8caf9e9dfb1
TUMO Labs <tumolabs@TUMOs-iMac.local>
2024-08-19 21:18:18 +0400

    slightly corrected the design

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -8,9 +8,12 @@ function Body() {
         
       <div className='flex justify-between px-[2%]'>
             <div className=''> div1 </div>
-            <div className='w-[950px] h-[310px]'>
-                   
-                  <span>Be Prepared For The Mountains And Beyond!</span>
+            <div className='w-[950px] h-[310px] flex flex-col'>
+                  <div className='flex items-center'>
+                    <hr className='w-[72px] border-[#FBD784]' /> <span className='mx-[32px] text-[#FBD784] text-[18px]'> A HIKING GUIDE</span>
+                  </div> 
+                  <span className='text-[88px] my-[32px] '>Be Prepared For The Mountains And Beyond!</span>
+                  <span>scroll down</span>
             </div>
             <div className=''> div3 </div> 
       </div> 
