e62982e0fea48b4950d2ac7fbbf2ed82d38c7883
TUMO Labs <tumolabs@TUMOs-iMac.local>
2024-09-05 16:47:10 +0400

    we are working on responsiveness

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,23 +4,23 @@ function Content() {
   return (
     
     <>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center gap-x-48">
             
-            <div className="flex items-center w-[1462px] h-[720px]" >
+            <div className="flex items-center justify-between w-[1462px] h-[720px]" >
                 <div className="h-[439px] w-[632px] flex flex-col gap-[27px]">
                     <div className='flex items-center'><hr className='w-[72px] border-[#FBD784]' /> <span className="mx-[32px] font-bold text-[18px] text-[#FBD784]">GET STARTED</span></div>
                     <span className="text-[64px]">What level of hiker are you?</span>
                     <p className="font-bold text-[18px]">Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker?</p>
                     <span className="font-bold text-[18px] text-[#FBD784]">read more</span>   
                 </div>
-                <div className="w-[566px] h-[720px] ml-[50px]">
+                <div className="w-[566px] h-[720px]">
                     <img className="w-full h-auto" src="/01.png" alt="" />
                 </div>
             </div>
 
-            <div className="flex items-center justify-between w-[1462px] h-[720px] mt-[200px] gap-x-10" >
+            <div className="flex items-center justify-between w-[1462px] h-[720px]" >
                
-                <div className="w-[566px] h-[720px] ml-[50px] ">
+                <div className="w-[566px] h-[720px] ">
                     <img className="w-full h-auto" src="/01.png" alt="" />
                 </div>
 
@@ -31,11 +31,25 @@ function Content() {
                     <span className="font-bold text-[18px] text-[#FBD784]">read more</span>   
                 </div>
             </div>
+
+            <div className="flex items-center justify-between w-[1462px] h-[720px]" >
+               
+               <div className="w-[566px] h-[720px] ">
+                   <img className="w-full h-auto" src="/01.png" alt="" />
+               </div>
+
+               <div className="h-[439px] w-[632px] flex flex-col gap-[27px]">
+                   <div className='flex items-center'><hr className='w-[72px] border-[#FBD784]' /> <span className="mx-[32px] font-bold text-[18px] text-[#FBD784]">GET STARTED</span></div>
+                   <span className="text-[64px]">What level of hiker are you?</span>
+                   <p className="font-bold text-[18px]">Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker?</p>
+                   <span className="font-bold text-[18px] text-[#FBD784]">read more</span>   
+               </div>
+           </div>
 
         </div>
     </>
   
     )
-}
+} 
 
 export default Content
